2eaabe4bbf7d3983807feec357b34b13df9df76d
dilini1995s <dilinitharanga721@gmail.com>
2020-04-24 09:23:46 +0530

    Initial commit

---
+++ src/test/java/com/example/SpringSQL/SpringSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
